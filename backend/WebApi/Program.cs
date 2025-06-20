9216ae3262487ce16feec6eed12ea9ab2b5c0d44
Sebastian Grønbech <115935487+SebastianGronbech@users.noreply.github.com>
2025-06-21 00:45:32 +0200

    feat(infrastructure): integrate Application and Infrastructure services in Program.cs

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -1,8 +1,11 @@
 using OsloOrbit.Application;
+using OsloOrbit.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApplication();
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
