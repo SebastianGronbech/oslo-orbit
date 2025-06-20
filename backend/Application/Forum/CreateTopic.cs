06410cb41a02616b9bb84c362596ef5d29c49e0c
Sebastian Grønbech <115935487+SebastianGronbech@users.noreply.github.com>
2025-06-20 20:40:21 +0200

    feat(forum): add Create Topic use case

---
+++ backend/Application/Forum/CreateTopic.cs
@@ -0,0 +1,27 @@
+using FluentResults;
+using MediatR;
+using OsloOrbit.Domain.Forum;
+
+namespace OsloOrbit.Application.Forum;
+
+public class CreateTopic
+{
+    public record Command(string Title) : IRequest<Result<Guid>>;
+
+    public class Handler : IRequestHandler<Command, Result<Guid>>
+    {
+        private readonly ITopicRepository _topicRepository;
+
+        public Handler(ITopicRepository topicRepository)
+        {
+            _topicRepository = topicRepository;
+        }
+
+        public async Task<Result<Guid>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var topic = Topic.Create(Guid.NewGuid(), request.Title);
+            await _topicRepository.AddAsync(topic, cancellationToken);
+            return Result.Ok(topic.Id);
+        }
+    }
+}
